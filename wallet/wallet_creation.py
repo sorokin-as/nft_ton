e6943cafd646dac8fcf1dbd1a94e2a4cc7336724
Sorokin Andrey <tasks.sorokin@gmail.com>
2025-03-14 16:44:55 +0300

    Пока ошибка к ЛС возможно это версия питона ша поставлю другую версию и продолжу

--- wallet/wallet_creation.py
+++ wallet/wallet_creation.py
@@ -9,6 +9,7 @@ mnemonics =  ['garlic', 'deal', 'hood', 'rocket', 'essay', 'noble', 'during', 'e
 
 mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(mnemonics=mnemonics, version=WalletVersionEnum.v3r2, workchain=0)
 
+wallet_address = wallet.address.to_string(True, True, True, True)
 
 
 
