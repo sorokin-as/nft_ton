b10ae22f7c0599063f8c7aad2d1902ce5896d284
Sorokin Andrey <tasks.sorokin@gmail.com>
2025-03-19 16:22:38 +0300

    Сделал создание и сохранения инфу в файл

--- TonWallet.py
+++ TonWallet.py
@@ -1,19 +1,33 @@
 from tonsdk.contract.wallet import Wallets, WalletVersionEnum
 
 class CryptoTonWallet:
-    def __init__(self, owner_name, initial_balance=0):
+    def __init__(self):
+        
+        self.wallet_address = self.create_wallet()
         #self.owner_name = owner_name
         #self.balance = initial_balance
-        self.wallet_address = self.generate_wallet_address()
+        #self.wallet_address = self.generate_wallet_address()
         #self.mnemonic_phrase = self.generate_mnemonic_phrase()
+        print('INIT CryptoTonWallet')
 
     def create_wallet(self):
+        # Создаем кошелек
         mnemonics, pub_k, priv_k, wallet = Wallets.create(version=WalletVersionEnum.v3r2, workchain=0)
         wallet_address = wallet.address.to_string(True, True, True, True)
+
+        # Сохраняем параметры в текстовый файл
+        with open(f"{wallet_address}.txt", "w") as file:
+            file.write(f"Мнемоники: {mnemonics}\n")
+            file.write(f"Публичный ключ: {pub_k}\n")
+            file.write(f"Приватный ключ: {priv_k}\n")
+            file.write(f"Адрес кошелька: {wallet_address}\n")
+
         return wallet_address
 
     def load_wallet_from_mnemonics(self):
-        return mnemo.generate(strength=128)  # 128 бит = 12 слов
+        mnemonics, pub_k, priv_k, wallet = Wallets.from_mnemonics(mnemonics=mnemonics, version=WalletVersionEnum.v3r2, workchain=0)
+        wallet_address = wallet.address.to_string(True, True, True, True)
+        return wallet_address
 
     def deposit(self, amount):
         if amount > 0:
@@ -40,19 +54,19 @@ class CryptoTonWallet:
             "mnemonic_phrase": self.mnemonic_phrase
         }
 
-# Пример использования
-if __name__ == "__main__":
-    # Создаем кошелек для пользователя "Alice" с начальным балансом 100
-    alice_wallet = CryptoWallet("Alice", 100)
+# # Пример использования
+# if __name__ == "__main__":
+#     # Создаем кошелек для пользователя "Alice" с начальным балансом 100
+#     alice_wallet = CryptoWallet("Alice", 100)
 
-    # Выводим информацию о кошельке
-    print(alice_wallet.get_wallet_info())
+#     # Выводим информацию о кошельке
+#     print(alice_wallet.get_wallet_info())
 
-    # Депозит на кошелек
-    alice_wallet.deposit(50)
+#     # Депозит на кошелек
+#     alice_wallet.deposit(50)
 
-    # Снятие с кошелька
-    alice_wallet.withdraw(30)
+#     # Снятие с кошелька
+#     alice_wallet.withdraw(30)
 
-    # Выводим обновленную информацию о кошельке
-    print(alice_wallet.get_wallet_info())
+#     # Выводим обновленную информацию о кошельке
+#     print(alice_wallet.get_wallet_info())
--- run.py
+++ run.py
@@ -1,12 +1,21 @@
 from TonWallet import CryptoTonWallet
 
-FirstWallet =  CryptoTonWallet
 
-while True:
-    user_input = input("Введите сообщение (или 0 для выхода): ")
-    
-    if user_input == "0":
-        print("Выход из цикла.")
-        break
-    
-    print(f"Вы ввели: {user_input}")а
+
+
+def startWallet():
+    while True:
+        user_input = input("Введите сообщение (или 0 для выхода): ")
+        
+        if user_input == "0":
+            print("Выход из цикла.")
+            break
+        
+        print(f"Вы ввели: {user_input}")
+
+
+
+
+if __name__ == "__main__":
+    FirstWallet =  CryptoTonWallet()
+    #startWallet()
